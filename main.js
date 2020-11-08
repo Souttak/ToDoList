02aba6a23ff9d9d3f4a982b5fb663b1ba96bf4db
Souttak <facundo.delaporte@gmail.com>
2020-11-08 18:50:25 -0300

    Added addTask function. Minor style changes

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+var counter = 0;
+
 function addTask(){
-    alert("Test");
+    var taskList = document.getElementById("taskList");
+    var taskInput = document.getElementById("taskInput");
+
+    const checkbox = "<input type=\"checkbox\" name=\"taskDone\" class=\"taskCheckBox\">";
+    const deleteButton = "<button class=\"deleteTaskButton\"><img src=\"/img/bx-trash.svg\"></button>";
+
+    var counter;
+
+    if(taskInput.value != "" && taskInput.value != null){
+        var newTask = document.createElement("li");
+        newTask.innerHTML = checkbox + taskInput.value + deleteButton;
+        taskList.appendChild(newTask);
+    }
+}
+
+function removeTask(){
+    var taskList = document.getElementById("taskList");
 }
