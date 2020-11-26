badbbfdf9bf3de4c20f35d0b819007fe3f3b6d65
Souttak <facundo.delaporte@gmail.com>
2020-11-26 19:10:12 -0300

    Now tasks are persistent

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+window.localStorage;
+
 /* 
     Sé que hacer esto no es muy buena práctica, y es conveniente usar objetos generados con
     document.createElement porque es mejor y más dinámico, pero para algo tan simple tampoco 
@@ -16,9 +18,23 @@ function addTask(){
         var newTask = document.createElement("li");
         newTask.innerHTML = checkbox + taskInput.value + "<div class='liButtonsDiv'>" + copyButton + shareButton + deleteButton + "</div>";
         taskList.appendChild(newTask);
+        saveTasks(taskList);
     }
 }
 
 function removeTask(task){
     task.closest("li").remove();
+    saveTasks(document.getElementById("taskList"));
+}
+
+function saveTasks(taskList){
+    localStorage.setItem("tasks", taskList.innerHTML);
+}
+
+function loadTasks(){
+    var taskList = document.getElementById("taskList");
+    var storedTaskList = localStorage.getItem("tasks");
+    if(storedTaskList != null){
+        taskList.innerHTML = storedTaskList;
+    }
 }
