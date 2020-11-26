84823cf1a84d7c313dbd9ed27c8b79ecb9dc8f0f
Souttak <facundo.delaporte@gmail.com>
2020-11-25 21:13:02 -0300

    Added share, copy and fullscreen buttons

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
+/* 
+    Sé que hacer esto no es muy buena práctica, y es conveniente usar objetos generados con
+    document.createElement porque es mejor y más dinámico, pero para algo tan simple tampoco 
+    pasa nada, y me ahorra trabajo.
+*/
+const checkbox = "<input type='checkbox' name='taskDone' class='taskCheckBox'>";
+const deleteButton = "<button class='deleteTaskButton'><img class='buttonIMG' src='/img/bx-trash.svg' onclick='removeTask(this)'></button>";
+const shareButton = "<button class='shareTaskButton'><img class='buttonIMG' src='/img/bx-share-alt.svg' onclick=''></button>";
+const copyButton = "<button class='copyTaskButton'><img class='buttonIMG' src='/img/bx-clipboard.svg' onclick=''></button>";
+
 function addTask(){
     var taskList = document.getElementById("taskList");
     var taskInput = document.getElementById("taskInput");
 
-    /* 
-        Sé que hacer esto no es muy buena práctica, y es conveniente usar objetos generados con
-        document.createElement porque es mejor y más dinámico, pero para algo tan simple tampoco 
-        pasa nada, y me ahorra trabajo.
-    */
-    const checkbox = "<input type='checkbox' name='taskDone' class='taskCheckBox'>";
-    const deleteButton = "<button class='deleteTaskButton'><img src='/img/bx-trash.svg' onclick='removeTask(this)'></button>";
-
     if(taskInput.value != "" && taskInput.value != null){
         var newTask = document.createElement("li");
-        newTask.innerHTML = checkbox + taskInput.value + deleteButton;
+        newTask.innerHTML = checkbox + taskInput.value + "<div class='liButtonsDiv'>" + copyButton + shareButton + deleteButton + "</div>";
         taskList.appendChild(newTask);
     }
 }
