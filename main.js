2d76094001cb7740e71a3a6299af84aa70427eb8
Souttak <facundo.delaporte@gmail.com>
2020-11-29 16:06:20 -0300

    Added fullscreen feature

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 window.localStorage;
 
 /* 
-    Sé que hacer esto no es muy buena práctica, y es conveniente usar objetos generados con
-    document.createElement porque es mejor y más dinámico, pero para algo tan simple tampoco 
-    pasa nada, y me ahorra trabajo.
+Sé que hacer esto no es muy buena práctica, y es conveniente usar objetos generados con
+document.createElement porque es mejor y más dinámico, pero para algo tan simple tampoco 
+pasa nada, y me ahorra trabajo.
 */
 const checkbox = "<input type='checkbox' name='taskDone' class='taskCheckBox'>";
 const deleteButton = "<button class='deleteTaskButton'><img class='buttonIMG' src='/img/bx-trash.svg' onclick='removeTask(this)'></button>";
 const shareButton = "<button class='shareTaskButton'><img class='buttonIMG' src='/img/bx-share-alt.svg' onclick='shareTask(this)'></button>";
 const copyButton = "<button class='copyTaskButton'><img class='buttonIMG' src='/img/bx-clipboard.svg' onclick='copyTask(this)'></button>";
+
+var isFullscreenEnabled = false;
 
 function addTask(){
     var taskList = document.getElementById("taskList");
@@ -46,4 +48,20 @@ function shareTask(reference){
 function copyTask(reference){
     var taskText = reference.closest("li").innerText;
     navigator.clipboard.writeText(taskText);
+}
+
+function fullscreen(reference){
+    if(document.fullscreenEnabled){
+        if(isFullscreenEnabled){
+            document.exitFullscreen();
+            reference.closest("header").innerHTML = "<h1>TASK LIST</h1>\n<button class='fullscreenButton'><img class='buttonIMG' src='/img/bx-fullscreen.svg' onclick='fullscreen(this)'></button>";
+            isFullscreenEnabled = !isFullscreenEnabled;
+        }else{
+            document.getElementById("taskListMainPage").requestFullscreen();
+            reference.closest("header").innerHTML = "<h1>TASK LIST</h1>\n<button class='fullscreenButton'><img class='buttonIMG' src='/img/bx-exit-fullscreen.svg' onclick='fullscreen(this)'></button>";
+            isFullscreenEnabled = !isFullscreenEnabled;
+        }
+    }else{
+        alert("Your browser doesn't support fullscreen mode.");
+    }
 }
